39f559469d7b70c6c66e27563e8687285ed256f3
bkaganovich <bkgnovich@gmail.com>
2023-01-08 15:36:53 +0200

    update

--- i3status/i3mod.sh
+++ i3status/i3mod.sh
@@ -4,21 +4,19 @@ i3status --config $HOME/.config/i3status/config | while :
 do
 	read line
    
-    watch -n 1 $HOME/.config/i3status/keyboard.sh &
+	watch -n 1 $HOME/.config/i3status/keyboard.sh &
 
-    # Get the current keyboard layout
-    #layout=$(xkb-switch)
+	# Get the current keyboard layout
+	#layout=$(xkb-switch)
 
-    # Check the layout and print the appropriate message
-    #if [ "$layout" == "il" ]; then
-    #    echo "il" > .$HOME/.config/i3status/keyboard.cache
-    #elif [ "$layout" == "us" ]; then
-    #    echo "us" > $HOME/.config/i3status/.keyboard.cache
-    #else
-    #    echo "$layout" > $HOME/.config/i3status/.keyboard.cache
-    #fi
-
-
+	# Check the layout and print the appropriate message
+	#if [ "$layout" == "il" ]; then
+	#	echo "il" > .$HOME/.config/i3status/keyboard.cache
+	#elif [ "$layout" == "us" ]; then
+	#	echo "us" > $HOME/.config/i3status/.keyboard.cache
+	#else
+	#	echo "$layout" > $HOME/.config/i3status/.keyboard.cache
+	#fi
 
 	echo "$(curl -s ifconfig.me)" > $HOME/.config/i3status/.ip.cache
 
